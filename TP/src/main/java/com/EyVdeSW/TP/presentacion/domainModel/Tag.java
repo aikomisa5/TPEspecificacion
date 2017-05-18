728b62ce96e8250b412eb59e69786b121c6833a7
Escritorio <Escritorio@Escritorio-PC>
2017-05-18 16:35:42 -0300

    Agregado equals

--- TP/src/main/java/com/EyVdeSW/TP/presentacion/domainModel/Tag.java
+++ TP/src/main/java/com/EyVdeSW/TP/presentacion/domainModel/Tag.java
@@ -53,6 +53,50 @@ public class Tag
 	{
 		this.accionesGenerales = accionesGenerales;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((accionesGenerales == null) ? 0 : accionesGenerales.hashCode());
+		result = prime * result + ((hijos == null) ? 0 : hijos.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((padre == null) ? 0 : padre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tag other = (Tag) obj;
+		if (accionesGenerales == null) {
+			if (other.accionesGenerales != null)
+				return false;
+		} else if (!accionesGenerales.equals(other.accionesGenerales))
+			return false;
+		if (hijos == null) {
+			if (other.hijos != null)
+				return false;
+		} else if (!hijos.equals(other.hijos))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (padre == null) {
+			if (other.padre != null)
+				return false;
+		} else if (!padre.equals(other.padre))
+			return false;
+		return true;
+	}
+	
 	
 
 }
