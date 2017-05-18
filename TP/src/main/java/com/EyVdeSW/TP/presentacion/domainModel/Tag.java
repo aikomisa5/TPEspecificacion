7980b5d845dd638d73116353501fb8146d4fb7bb
Rober <Rober@Rober-NB>
2017-05-18 11:09:37 -0300

    renamed atributo padres -> padre

--- TP/src/main/java/com/EyVdeSW/TP/presentacion/domainModel/Tag.java
+++ TP/src/main/java/com/EyVdeSW/TP/presentacion/domainModel/Tag.java
@@ -4,11 +4,51 @@ import java.util.List;
 
 public class Tag
 {
-	private Tag padres;
+	private Tag padre;
 	private List<Tag> hijos;
 	private String nombre;
 	
 	private List<AccionGeneral> accionesGenerales;
+
+	public Tag getPadres()
+	{
+		return padre;
+	}
+
+	public void setPadres(Tag padres)
+	{
+		this.padre = padres;
+	}
+
+	public List<Tag> getHijos()
+	{
+		return hijos;
+	}
+
+	public void setHijos(List<Tag> hijos)
+	{
+		this.hijos = hijos;
+	}
+
+	public String getNombre()
+	{
+		return nombre;
+	}
+
+	public void setNombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
+
+	public List<AccionGeneral> getAccionesGenerales()
+	{
+		return accionesGenerales;
+	}
+
+	public void setAccionesGenerales(List<AccionGeneral> accionesGenerales)
+	{
+		this.accionesGenerales = accionesGenerales;
+	}
 	
 
 }
