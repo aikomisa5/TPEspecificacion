8bd8425b29785291390846f76f41f4419f2dfa0a
Escritorio <Escritorio@Escritorio-PC>
2017-05-20 14:42:16 -0300

    Creado ArbolTag

---
+++ TP/src/main/java/com/EyVdeSW/TP/domainModel/ArbolTag.java
@@ -0,0 +1,45 @@
+package com.EyVdeSW.TP.domainModel;
+
+public class ArbolTag 
+{
+	private Tag raiz;
+	
+	public ArbolTag(Tag raiz) {
+		this.raiz=raiz;
+	}
+
+	public Tag getRaiz() {
+		return raiz;
+	}
+
+	public void setRaiz(Tag raiz) {
+		this.raiz = raiz;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((raiz == null) ? 0 : raiz.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ArbolTag other = (ArbolTag) obj;
+		if (raiz == null) {
+			if (other.raiz != null)
+				return false;
+		} else if (!raiz.equals(other.raiz))
+			return false;
+		return true;
+	}
+
+	
+}
--- TP/src/main/java/com/EyVdeSW/TP/domainModel/Tag.java
+++ TP/src/main/java/com/EyVdeSW/TP/domainModel/Tag.java
@@ -4,98 +4,90 @@ import java.util.List;
 
 public class Tag
 {
-	private NodoTag raiz;
+	private List<Tag> hijos;
+	private String nombre;
+	private List<AccionGeneral>	accionesGenerales;
+
+	public Tag(List<Tag> hijos, String nombre, List<AccionGeneral> accionesGenerales) {
+		super();
+		this.hijos = hijos;
+		this.nombre = nombre;
+		this.accionesGenerales = accionesGenerales;
+	}
 	
-	
-	public NodoTag getRaiz() {
-		return raiz;
+	public Tag(String nombre) {
+		super();
+		this.nombre = nombre;
+		this.hijos=null;
+		this.accionesGenerales=null;
 	}
 
-	public void setRaiz(NodoTag raiz) {
-		this.raiz = raiz;
+	public List<Tag> getHijos()
+	{
+		return hijos;
 	}
-	
-	public class NodoTag{
-		
-		private List<NodoTag> hijos;
-		private String nombre;
-		private List<AccionGeneral>	accionesGenerales;
-		
-		public NodoTag(List<NodoTag> hijos, String nombre, List<AccionGeneral> accionesGenerales) {
-			this.hijos = hijos;
-			this.nombre = nombre;
-			this.accionesGenerales = accionesGenerales;
-		}
-		
-		public NodoTag(String nombre) {
-			this.nombre = nombre;
-			this.hijos=null;
-			this.accionesGenerales=null;
-		}
 
-		public List<NodoTag> getHijos(){
-			return hijos;
-		}
+	public void setHijos(List<Tag> hijos)
+	{
+		this.hijos = hijos;
+	}
 
-		public void setHijos(List<NodoTag> hijos){
-			this.hijos = hijos;
-		}
+	public String getNombre() {
+		return nombre;
+	}
 
-		public String getNombre() {
-			return nombre;
-		}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
+	public List<AccionGeneral> getAccionesGenerales()
+	{
+		return accionesGenerales;
+	}
 
-		public List<AccionGeneral> getAccionesGenerales(){
-			return accionesGenerales;
-		}
+	public void setAccionesGenerales(List<AccionGeneral> accionesGenerales)
+	{
+		this.accionesGenerales = accionesGenerales;
+	}
 
-		public void setAccionesGenerales(List<AccionGeneral> accionesGenerales){
-			this.accionesGenerales = accionesGenerales;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			NodoTag other = (NodoTag) obj;
-			if (!getOuterType().equals(other.getOuterType()))
-				return false;
-			if (accionesGenerales == null) {
-				if (other.accionesGenerales != null)
-					return false;
-			} else if (!accionesGenerales.equals(other.accionesGenerales))
-				return false;
-			if (hijos == null) {
-				if (other.hijos != null)
-					return false;
-			} else if (!hijos.equals(other.hijos))
-				return false;
-			if (nombre == null) {
-				if (other.nombre != null)
-					return false;
-			} else if (!nombre.equals(other.nombre))
-				return false;
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
 			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tag other = (Tag) obj;
+		if (accionesGenerales == null)
+		{
+			if (other.accionesGenerales != null)
+				return false;
 		}
-
-		@Override
-		public String toString(){
-			return "Tag: " + nombre;
+		else if (!accionesGenerales.equals(other.accionesGenerales))
+			return false;
+		if (hijos == null)
+		{
+			if (other.hijos != null)
+				return false;
 		}
-
-		private Tag getOuterType() {
-			return Tag.this;
+		else if (!hijos.equals(other.hijos))
+			return false;
+		if (nombre == null)
+		{
+			if (other.nombre != null)
+				return false;
 		}
-		
+		else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
 	}
 
+	@Override
+	public String toString()
+	{
+		return "Tag: " + nombre;
+	}
 
 }
