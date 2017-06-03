863929f78df13331719c135cad977a246f6fd229
aikomisa5 <misacinco@gmail.com>
2017-06-03 00:50:42 -0300

    metodos en interface mensajeDAO y nombre en clase mensaje

--- TP/src/main/java/com/EyVdeSW/TP/Daos/MensajeDAO.java
+++ TP/src/main/java/com/EyVdeSW/TP/Daos/MensajeDAO.java
@@ -1,9 +1,14 @@
 package com.EyVdeSW.TP.Daos;
 
+import java.util.Collection;
+
 import com.EyVdeSW.TP.domainModel.Mensaje;
 
 public interface MensajeDAO extends DAO<Mensaje>{
 	
 	public void modificar (String mensajeOriginal, String mensajeModificacion);
-
+	public Collection<Mensaje>traerTodosLosMensajes();
+	public Collection<Mensaje>consultarPorNombre(String nombreMensaje);
+	public Mensaje getMensajePorNombre(String nombreMensaje);
+	public boolean existe(String nombreMensaje);
 }
--- TP/src/main/java/com/EyVdeSW/TP/domainModel/Mensaje.java
+++ TP/src/main/java/com/EyVdeSW/TP/domainModel/Mensaje.java
@@ -2,10 +2,16 @@ package com.EyVdeSW.TP.domainModel;
 
 public class Mensaje {
 	
+	//TODO
+	private String nombre; //Para identificar al mensaje
+							
+							//O conviene un id?
+	
 	private String textoMensaje;
 
-	public Mensaje(String textoMensaje)
+	public Mensaje(String nombre,String textoMensaje)
 	{
+		this.nombre=nombre;
 		this.textoMensaje=textoMensaje;
 	}
 	
