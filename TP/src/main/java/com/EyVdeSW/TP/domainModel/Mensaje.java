c6da5330280e75115a6a953e8f50349cfa0535d2
aikomisa5 <misacinco@gmail.com>
2017-06-02 23:02:21 -0300

    clase Mensaje

---
+++ TP/src/main/java/com/EyVdeSW/TP/domainModel/Mensaje.java
@@ -0,0 +1,12 @@
+package com.EyVdeSW.TP.domainModel;
+
+public class Mensaje {
+	
+	private String textoMensaje;
+
+	public Mensaje(String textoMensaje)
+	{
+		this.textoMensaje=textoMensaje;
+	}
+	
+}
