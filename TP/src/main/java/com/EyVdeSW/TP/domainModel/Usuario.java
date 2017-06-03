5b65f981e9641e7425e197b81621266c53003111
aikomisa5 <misacinco@gmail.com>
2017-06-03 01:09:09 -0300

    clase Usuario

---
+++ TP/src/main/java/com/EyVdeSW/TP/domainModel/Usuario.java
@@ -0,0 +1,33 @@
+package com.EyVdeSW.TP.domainModel;
+
+public class Usuario {
+	
+	private String usuario;
+	private String nombre;
+	private String password;
+	
+	public Usuario (String usuario, String nombre, String password){
+		
+		this.usuario=usuario;
+		this.nombre=nombre;
+		this.password=password;
+		
+	}
+	
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+}
