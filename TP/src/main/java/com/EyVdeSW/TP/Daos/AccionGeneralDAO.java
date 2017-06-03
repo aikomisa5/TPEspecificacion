5774764bd3fe9fac5d855000024f37b0502e58c4
aikomisa5 <misacinco@gmail.com>
2017-06-03 00:41:03 -0300

    más metodos en AccionGeneralDAO

--- TP/src/main/java/com/EyVdeSW/TP/Daos/AccionGeneralDAO.java
+++ TP/src/main/java/com/EyVdeSW/TP/Daos/AccionGeneralDAO.java
@@ -1,9 +1,15 @@
 package com.EyVdeSW.TP.Daos;
 
+import java.util.Collection;
+
 import com.EyVdeSW.TP.domainModel.AccionGeneral;
+import com.EyVdeSW.TP.domainModel.Campaña;
 
 public interface AccionGeneralDAO extends DAO<AccionGeneral> {
 
 	public void modificar (AccionGeneral original, AccionGeneral modificacion);
-	
+	public Collection<AccionGeneral>traerTodos();
+	public Collection<AccionGeneral>consultarPorNombre(String nombreAccionGeneral);
+	public AccionGeneral getAccionGeneralPorNombre(String nombreAccionGeneral);
+	public boolean existe(String nombreAccionGeneral);
 }
