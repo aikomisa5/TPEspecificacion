fa64b79b003353c6dbbab6b43845c9e965738b63
aikomisa5 <misacinco@gmail.com>
2017-06-05 14:32:53 -0300

    testUsuarioDAONeodatis (not yet implemented)

---
+++ TP/src/main/java/com/EyVdeSW/TP/Daos/impl/TestUsuarioDAONeodatis.java
@@ -0,0 +1,14 @@
+package com.EyVdeSW.TP.Daos.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUsuarioDAONeodatis {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
