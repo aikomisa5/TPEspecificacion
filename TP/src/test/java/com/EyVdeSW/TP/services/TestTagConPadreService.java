7b856bbead2c7cb85c36233866df067b9ce5ec35
Danilo Eidelson <Danilo>
2017-06-09 10:55:55 -0300

    Testeado el service de tag padre(100%)

--- TP/src/test/java/com/EyVdeSW/TP/services/TestTagConPadreService.java
+++ TP/src/test/java/com/EyVdeSW/TP/services/TestTagConPadreService.java
@@ -25,6 +25,37 @@ public class TestTagConPadreService {
 	
 	
 	@Test
+	public void guardar(){
+		TagConPadre t1=new TagConPadre("t1");
+		tagDAO.guardar(t1);
+		replay(tagDAO);
+		service.guardar(t1);
+		verify(tagDAO);
+	}
+	
+	@Test
+	public void guardarComplejo(){
+		TagConPadre t1=new TagConPadre("t1");
+		TagConPadre padre=new TagConPadre("padre");
+		EasyMock.expect(tagDAO.getTagPorNombre(padre.getNombre())).andReturn(padre);
+		EasyMock.expect(tagDAO.existe(t1.getNombre())).andReturn(false);
+		tagDAO.guardar(t1);
+		replay(tagDAO);
+		service.guardar(t1.getNombre(), padre.getNombre());
+		verify(tagDAO);
+	}
+	
+	@Test
+	public void borrar(){
+		TagConPadre t1=new TagConPadre("t1");
+		EasyMock.expect(tagDAO.getTagPorNombre(t1.getNombre())).andReturn(t1);
+		tagDAO.borrar(t1);
+		replay(tagDAO);
+		service.borrar(t1.getNombre());
+		verify(tagDAO);
+	}
+	
+	@Test
 	public void testTraerTodos() {
 		List<TagConPadre>tags= new ArrayList<>();
 		tags.add(new TagConPadre("t1"));
@@ -39,8 +70,9 @@ public class TestTagConPadreService {
 	}
 	
 	@Test
-	public void modificar(){
+	public void modificarHP(){
 		//TODO lean esto
+		//Happy path
 		TagConPadre original=new TagConPadre("original");
 		TagConPadre modificacion=new TagConPadre("modificacion");
 		//paso 1 ejecutamos todos los metodos del dao que aparecen en el service
@@ -53,12 +85,20 @@ public class TestTagConPadreService {
 		
 		//activamos el mock object en este caso el dao
 		replay(tagDAO);
-
 		//testeamos la funcionalidad
 		assertTrue(service.modificar(original.getNombre(), modificacion.getNombre()));
-		
 		//verificamos si se hizo el llamado al mock object
 		verify(tagDAO);
+	}
+	
+	@Test
+	public void modificarElse(){
+		TagConPadre original=new TagConPadre("original");
+		TagConPadre modificacion=new TagConPadre("modificacion");
+		//ahora testeo el caso de que exista el tag (no deberian ejecutarse los demas metodos
+		EasyMock.expect(tagDAO.existe(modificacion.getNombre())).andReturn(true);
+		replay(tagDAO);
+		assertFalse(service.modificar(original.getNombre(), modificacion.getNombre()));
 	}
 
 }
