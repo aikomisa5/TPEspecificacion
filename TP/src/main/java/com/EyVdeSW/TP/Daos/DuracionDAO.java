f65e5b8f7575e04abb5e78585b9c2910c6db6042
Rober Gerszenswit <rober.gerszen@gmail.com>
2017-06-16 12:01:51 -0300

    fixed nombres

--- TP/src/main/java/com/EyVdeSW/TP/Daos/DuracionDAO.java
+++ TP/src/main/java/com/EyVdeSW/TP/Daos/DuracionDAO.java
@@ -7,9 +7,8 @@ import com.EyVdeSW.TP.domainModel.Mensaje;
 
 public interface DuracionDAO extends DAO<Duracion>{
 	
-	public void modificar (String descripcion , Duracion Duracion);
-	public Collection<Duracion>traerTodosLosMensajes();
-	public Collection<Duracion>consultarPorNombre(String nombreMensaje);
-	public Mensaje getMensajePorNombre(String nombreMensaje);
-	public boolean existe(String nombreMensaje);
+	public void modificar (String descripcion , Duracion duracion);
+	public Collection<Duracion>traerDuraciones();	
+	public Mensaje getDuracionPorDescripcion(String descripcion);
+	public boolean existe(String descripcion);
 }
