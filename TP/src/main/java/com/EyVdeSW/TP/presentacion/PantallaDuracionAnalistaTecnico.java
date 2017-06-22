192f7ed18672cc8bc5579ecd897dab3faf31fa74
Rober Gerszenswit <rober.gerszen@gmail.com>
2017-06-22 12:08:02 -0300

    cleanup pantalla duracion

--- TP/src/main/java/com/EyVdeSW/TP/presentacion/PantallaDuracionAnalistaTecnico.java
+++ TP/src/main/java/com/EyVdeSW/TP/presentacion/PantallaDuracionAnalistaTecnico.java
@@ -1,25 +1,15 @@
 package com.EyVdeSW.TP.presentacion;
 
-import com.EyVdeSW.TP.domainModel.Tag;
 import com.EyVdeSW.TP.services.DuracionService;
-import com.EyVdeSW.TP.services.TagService;
-import com.vaadin.data.util.BeanItemContainer;
-import com.vaadin.data.util.HierarchicalContainer;
-import com.vaadin.event.ShortcutAction;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.ComboBox;
-import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
-import com.vaadin.ui.Tree;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.themes.ValoTheme;
 
 @SuppressWarnings("serial")
@@ -38,7 +28,7 @@ public class PantallaDuracionAnalistaTecnico extends VerticalLayout implements V
 		setComponentAlignment(hlTitulo, Alignment.MIDDLE_CENTER);
 
 		TextField tfDescripcion = new TextField("Descripcion");
-		//	TODO binding a grid.
+		// TODO binding a grid.
 	}
 
 	@Override
@@ -46,7 +36,5 @@ public class PantallaDuracionAnalistaTecnico extends VerticalLayout implements V
 		// TODO Auto-generated method stub
 
 	}
-
-
 
 }
