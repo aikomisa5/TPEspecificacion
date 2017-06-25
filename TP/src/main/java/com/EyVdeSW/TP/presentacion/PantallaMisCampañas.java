fad09d53335c3f0788b8a5b46cd3d0e7c0f4ad71
Rober Gerszenswit <rober.gerszen@gmail.com>
2017-06-25 10:33:57 -0300

    pantalla mis campañas sin logica creada

--- TP/src/main/java/com/EyVdeSW/TP/presentacion/PantallaMisCampañas.java
+++ TP/src/main/java/com/EyVdeSW/TP/presentacion/PantallaMisCampañas.java
@@ -10,8 +10,10 @@ import com.EyVdeSW.TP.services.CampañaService;
 import com.EyVdeSW.TP.services.UsuarioService;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.AbstractOrderedLayout;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
@@ -25,33 +27,39 @@ public class PantallaMisCampañas extends VerticalLayout implements View {
 	CampañaService campañaService = CampañaService.getCampañaService();
 	UsuarioService usuarioService = UsuarioService.getUsuarioService();
 	List <Campania> campañas = new ArrayList<>();
-	
-	String username = "";
+	Usuario usuario;
+	String username = "";	
+
+	private VerticalLayout contenedorCampañas;
 	
 	public PantallaMisCampañas(){
 		
 
 		Label titulo = new Label("Mis Campañas");
 		titulo.setStyleName(ValoTheme.LABEL_H1);
+		
+		
+		
 		HorizontalLayout hlTitulo = new HorizontalLayout(titulo);
 		addComponent(hlTitulo);
-		setComponentAlignment(hlTitulo, Alignment.MIDDLE_CENTER);
-		
-		Button editar = new Button("Editar campaña seleccionada");
-		Button borrar = new Button("Borrar campaña seleccionada");
-		
-		
+		setComponentAlignment(hlTitulo, Alignment.MIDDLE_CENTER);				
+		contenedorCampañas =  new VerticalLayout();	
+		contenedorCampañas.setSpacing(true);
+		contenedorCampañas.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		addComponent(contenedorCampañas);		
+		setComponentAlignment(contenedorCampañas, Alignment.MIDDLE_CENTER);
+				
 		
 	}
 
 	@Override
-	public void enter(ViewChangeEvent event) {
-		// TODO Auto-generated method stub
+	public void enter(ViewChangeEvent event) {		
 		String usernameMail = String.valueOf(getSession().getAttribute("user"));
 		username=usernameMail;
-		Usuario usuario = usuarioService.getUsuarioPorMail(username);
-		campañas = campañaService.getCampañasDeUsuario(usuario);
-		
+		usuario = usuarioService.getUsuarioPorMail(username);
+		campañas = campañaService.getCampañasDeUsuario(usuario);		
+		contenedorCampañas.removeAllComponents();
+		campañas.forEach(campaña -> contenedorCampañas.addComponent(new itemMisCampañas(campaña)));
 	}
 
 	
---
+++ TP/src/main/java/com/EyVdeSW/TP/presentacion/itemMisCampañas.java
@@ -0,0 +1,49 @@
+package com.EyVdeSW.TP.presentacion;
+
+import com.EyVdeSW.TP.domainModel.Campania;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.themes.ValoTheme;
+
+public class itemMisCampañas extends HorizontalLayout {
+	private Campania campaña;
+	private Label lblCampaña;	
+	private Button editar;
+	private Button pagar;
+	private Button cancelar;
+	private Button borrar;
+
+	public itemMisCampañas(Campania campaña) {
+		setWidth("700px");
+		this.campaña = campaña;
+		lblCampaña = new Label();
+		lblCampaña.setCaption("Nombre: " + campaña.getNombre() + "Descripción : " + campaña.getDescripcion());
+		
+		
+		editar = new Button();
+		editar.setIcon(FontAwesome.EDIT);		
+		editar.setDescription("Editar esta campaña");		
+		
+		pagar = new Button();
+		pagar.setIcon(FontAwesome.MONEY);
+		pagar.setDescription("Pagar esta campaña");
+		pagar.setStyleName(ValoTheme.BUTTON_FRIENDLY);
+		
+		cancelar = new Button();
+		cancelar.setIcon(FontAwesome.HAND_STOP_O);
+		cancelar.setDescription("Cancelar esta campaña");
+		cancelar.setStyleName(ValoTheme.BUTTON_DANGER);
+		
+		borrar = new Button();
+		borrar.setIcon(FontAwesome.TRASH);
+		borrar.setDescription("Borrar esta campaña");
+		borrar.setStyleName(ValoTheme.BUTTON_DANGER);
+		
+		lblCampaña.setWidth("100%");		
+		addComponents(lblCampaña, editar, pagar, cancelar, borrar);
+		setExpandRatio(lblCampaña, 1.0f);
+	}
+}
