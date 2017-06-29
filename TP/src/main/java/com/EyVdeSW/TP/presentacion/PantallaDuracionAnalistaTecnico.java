485d85c8259ed5ab442501654076771ccb2223ff
deidelson <deidelson@mail.com>
2017-06-29 19:13:20 -0300

    Construyendo pantalla de duraciones

--- TP/src/main/java/com/EyVdeSW/TP/presentacion/PantallaDuracionAnalistaTecnico.java
+++ TP/src/main/java/com/EyVdeSW/TP/presentacion/PantallaDuracionAnalistaTecnico.java
@@ -7,11 +7,15 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.Tree;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.themes.ValoTheme;
 
 @SuppressWarnings("serial")
@@ -34,13 +38,64 @@ public class PantallaDuracionAnalistaTecnico extends VerticalLayout implements V
 		addComponent(hlTitulo);
 		setComponentAlignment(hlTitulo, Alignment.MIDDLE_CENTER);
 
-		ComboBox d = new ComboBox("Duraciones", duraciones);
+		Tree arbolDuraciones = new Tree("Duraciones", duraciones);
 		
 		TextField tfDescripcion = new TextField("Descripcion");
 		TextField tfCantidadDias= new TextField("Cantidad dias");
-		this.addComponent(d);
+		Button guardar = new Button("Guardar");
+		guardar.addClickListener( event -> {
+			if(tfDescripcion.getValue() == null || tfCantidadDias.getValue() == null){
+				Notification.show("El nombre esta vacío!", Type.WARNING_MESSAGE);
+			}else{
+				try {
+					duracionService.guardar(tfDescripcion.getValue(), Integer.parseInt(tfCantidadDias.getValue()));
+					Notification.show("Duracion Guardada", Type.TRAY_NOTIFICATION);
+					limpiarActualizar(arbolDuraciones, tfDescripcion, tfCantidadDias, duraciones);
+				} catch (Exception e) {
+					e.printStackTrace();
+					Notification.show("Algun dato es erroneo", Type.WARNING_MESSAGE);
+				}
+			}
+		});
 		
 		
+		Button eliminar = new Button("Eliminar");
+		eliminar.addClickListener(event -> {
+			if(tfDescripcion.getValue() == null){
+				Notification.show("El nombre esta vacío!", Type.WARNING_MESSAGE);
+			}else{
+				if(duracionService.existe(tfDescripcion.getValue())){
+					duracionService.borrar(tfDescripcion.getValue());
+					Notification.show("El nombre esta vacío!", Type.WARNING_MESSAGE);
+					limpiarActualizar(arbolDuraciones, tfDescripcion, tfCantidadDias, duraciones);
+				}else{
+					Notification.show("No existe dicha duracion", Type.WARNING_MESSAGE);
+				}
+				
+			}
+		});
+		
+		Button editar = new Button("Editar");
+		
+		
+		arbolDuraciones.addItemClickListener(event ->{
+			Duracion duracion = duracionService.getDuracionPorDescripcion(event.getItemId().toString());
+			tfDescripcion.setValue(duracion.getDescripcion());
+			tfCantidadDias.setValue(Integer.toString(duracion.getDuracion()));
+		});
+		
+		HorizontalLayout hl = new HorizontalLayout(arbolDuraciones,tfDescripcion, tfCantidadDias);
+		
+		HorizontalLayout hl1 = new HorizontalLayout(guardar, editar, eliminar);
+		hl.setSpacing(true);
+		hl1.setSpacing(true);
+		VerticalLayout vl = new VerticalLayout(hl, hl1);
+		vl.setSpacing(true);
+	
+		
+		this.addComponent(vl);
+		setComponentAlignment(vl, Alignment.TOP_CENTER);
+		this.setSpacing(true);
 		// TODO binding a grid.
 	}
 
@@ -48,6 +103,17 @@ public class PantallaDuracionAnalistaTecnico extends VerticalLayout implements V
 	public void enter(ViewChangeEvent event) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public void limpiarActualizar(Tree tree, TextField tf1, TextField tf2, BeanItemContainer<Duracion>duraciones){
+		tf1.clear();
+		tf2.clear();
+		tree.removeAllItems();
+		duracionService.traerDuraciones().forEach(duracion -> {
+			duraciones.addBean(duracion);
+		});
+		tree.setContainerDataSource(duraciones);
+		
 	}
 
 }
