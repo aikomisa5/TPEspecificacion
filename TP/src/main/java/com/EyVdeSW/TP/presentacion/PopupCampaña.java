6584f50605ff8e410a0ee619db78e6201a682887
Rober Gerszenswit <rober.gerszen@gmail.com>
2017-06-29 15:52:19 -0300

    añadido popup para detalles de campaña

---
+++ TP/src/main/java/com/EyVdeSW/TP/presentacion/PopupCampaña.java
@@ -0,0 +1,48 @@
+package com.EyVdeSW.TP.presentacion;
+
+import com.EyVdeSW.TP.domainModel.Campania;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.PopupView;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
+public class PopupCampaña extends PopupView {
+	private String descripcionBreve;
+	private VerticalLayout mainLayout;
+	private Label nombre = new Label();
+	private TextArea descripcion= new TextArea("Descripción");
+	private TextArea mensaje= new TextArea("Mensaje");
+	private Label estado= new Label();
+	
+	public PopupCampaña(Campania campaña, VerticalLayout popupContent){
+		super("",popupContent);
+		this.mainLayout = new VerticalLayout();
+		this.mainLayout.addComponents(nombre,descripcion,mensaje,estado);		
+		updateDatos(campaña);
+		this.addContextClickListener(click -> this.setPopupVisible(true));
+		this.setHideOnMouseOut(false);
+		this.setWidth("600px");
+		popupContent.addComponent(this.mainLayout);
+		popupContent.setComponentAlignment(this.mainLayout, Alignment.MIDDLE_CENTER);
+		popupContent.setWidth("500px");
+		popupContent.setDefaultComponentAlignment(Alignment.TOP_CENTER);
+		nombre.setWidth("90%");
+		descripcion.setWidth("90%");
+		mensaje.setWidth("90%");
+		estado.setWidth("90%");
+		descripcion.setReadOnly(true);
+		mensaje.setReadOnly(true);		
+	}
+
+	public void updateDatos(Campania campaña) {
+		descripcionBreve = campaña.getNombre() + " Estado: " + campaña.getEstado().toString();
+		this.setCaption(descripcionBreve);
+		nombre.setCaption("Nombre: " + campaña.getNombre());
+		descripcion.setValue(campaña.getDescripcion());
+		mensaje.setValue(campaña.getMensaje().getTextoMensaje());
+		estado.setCaption("Estado: " + campaña.getEstado().toString());		
+	}
+
+}
--- TP/src/main/java/com/EyVdeSW/TP/presentacion/itemMisCampañas.java
+++ TP/src/main/java/com/EyVdeSW/TP/presentacion/itemMisCampañas.java
@@ -5,17 +5,21 @@ import com.EyVdeSW.TP.domainModel.Campania.EstadoCampania;
 import com.EyVdeSW.TP.services.CampañaService;
 import com.EyVdeSW.TP.services.AccionPublicitariaScheduler;
 import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Notification.Type;
+import com.vaadin.ui.PopupView;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
 public class itemMisCampañas extends HorizontalLayout {
 	private Campania campaña;
-	private Label lblCampaña;	
+	private PopupCampaña popupCampaña;
+	private Button verDetalles;
 	private Button editar;
 	private Button pagar;
 	private Button cancelar;
@@ -23,71 +27,76 @@ public class itemMisCampañas extends HorizontalLayout {
 	private CampañaService campañaService = CampañaService.getCampañaService();
 	private AccionPublicitariaScheduler ms = AccionPublicitariaScheduler.getMailScheduler();
 
-	public itemMisCampañas(Campania campaña) {		
+	public itemMisCampañas(Campania campaña) {
 		setWidth("700px");
 		this.campaña = campaña;
-		lblCampaña = new Label();
-		updateLblCampaña(campaña);
-		
-		
-		editar = new Button();
-		editar.setIcon(FontAwesome.EDIT);		
-		editar.setDescription("Editar esta campaña");
-		editar.addClickListener(e ->{
-			getUI().getNavigator().navigateTo(PantallaCampañaCliente.NAME + "/"+ campaña.getIdCampania().toString());
-			
+
+		popupCampaña = new PopupCampaña(campaña, new VerticalLayout());
+		popupCampaña.updateDatos(campaña);
+
+		verDetalles = new Button();
+		verDetalles.setIcon(FontAwesome.INFO_CIRCLE);
+		verDetalles.setStyleName(ValoTheme.BUTTON_QUIET + " " + ValoTheme.BUTTON_SMALL);
+		verDetalles.addClickListener(e -> {
+			popupCampaña.setPopupVisible(true);
 		});
-		
+
+		editar = new Button();
+		editar.setIcon(FontAwesome.EDIT);
+		editar.setDescription("Editar esta campaña");
+		editar.addClickListener(e -> {
+			getUI().getNavigator().navigateTo(PantallaCampañaCliente.NAME + "/" + campaña.getIdCampania().toString());
+
+		});
+
 		pagar = new Button();
 		pagar.setIcon(FontAwesome.MONEY);
 		pagar.setDescription("Pagar esta campaña");
 		pagar.setStyleName(ValoTheme.BUTTON_FRIENDLY);
-		pagar.addClickListener(e ->{
+		pagar.addClickListener(e -> {
 			campaña.setEstado(EstadoCampania.PLANIFICADA);
 			campañaService.modificar(campaña.getIdCampania(), campaña);
 			ms.agregarAccionesDeCampaña(campaña);
-			updateLblCampaña(campaña);
-			//TODO integrar a scheduler.
+			updateDatosCampaña(campaña);
+			// TODO integrar a scheduler.
 			Notification.show("Campaña Planificada", Type.TRAY_NOTIFICATION);
 			establecerVisibilidadBotones();
 		});
-		
-		
+
 		cancelar = new Button();
 		cancelar.setIcon(FontAwesome.HAND_STOP_O);
 		cancelar.setDescription("Cancelar esta campaña");
 		cancelar.setStyleName(ValoTheme.BUTTON_DANGER);
-		cancelar.addClickListener(e ->{
+		cancelar.addClickListener(e -> {
 			campaña.setEstado(EstadoCampania.CANCELADA);
 			campañaService.modificar(campaña.getIdCampania(), campaña);
 			ms.cancelarCampaña(campaña);
-			updateLblCampaña(campaña);
-			//TODO integrar a scheduler.
+			updateDatosCampaña(campaña);
+			// TODO integrar a scheduler.
 			Notification.show("Campaña Cancelada", Type.TRAY_NOTIFICATION);
 			establecerVisibilidadBotones();
 		});
-		
-		
+
 		borrar = new Button();
 		borrar.setIcon(FontAwesome.TRASH);
 		borrar.setDescription("Borrar esta campaña");
 		borrar.setStyleName(ValoTheme.BUTTON_DANGER);
-		borrar.addClickListener(e ->{
+		borrar.addClickListener(e -> {
 			campañaService.borrar(campaña.getIdCampania());
-			//TODO integrar a scheduler. 
+			// TODO integrar a scheduler.
 			Notification.show("Campaña Borrada", Type.TRAY_NOTIFICATION);
 			this.removeAllComponents();
 		});
-		
-		lblCampaña.setWidth("100%");		
-		addComponents(lblCampaña, editar, pagar, cancelar, borrar);
-		setExpandRatio(lblCampaña, 1.0f);
+
+		addComponents(verDetalles, popupCampaña, editar, pagar, cancelar, borrar);
+		setExpandRatio(popupCampaña, 1.0f);
+		setComponentAlignment(popupCampaña, Alignment.MIDDLE_LEFT);
 		
 		establecerVisibilidadBotones();
 	}
 
 	private void establecerVisibilidadBotones() {
-		switch (campaña.getEstado()){
+		switch (campaña.getEstado()) {
 		case CANCELADA:
 			editar.setEnabled(false);
 			pagar.setEnabled(false);
@@ -114,7 +123,7 @@ public class itemMisCampañas extends HorizontalLayout {
 		}
 	}
 
-	private void updateLblCampaña(Campania campaña) {
-		lblCampaña.setCaption("Nombre: " + campaña.getNombre() + " Descripción: " + campaña.getDescripcion() + "Estado: " + campaña.getEstado());
+	private void updateDatosCampaña(Campania campaña) {
+		popupCampaña.updateDatos(campaña);
 	}
 }
