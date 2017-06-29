d0a36498f702e26c5bb4efde060df28aeaa35006
Danilo Eidelson <deidelson@mail.com>
2017-06-29 18:12:02 -0300

    Empezando pantalla de duraciones

--- TP/src/main/java/com/EyVdeSW/TP/presentacion/PantallaDuracionAnalistaTecnico.java
+++ TP/src/main/java/com/EyVdeSW/TP/presentacion/PantallaDuracionAnalistaTecnico.java
@@ -1,10 +1,13 @@
 package com.EyVdeSW.TP.presentacion;
 
+import com.EyVdeSW.TP.domainModel.Duracion;
 import com.EyVdeSW.TP.services.DuracionService;
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
@@ -16,17 +19,28 @@ public class PantallaDuracionAnalistaTecnico extends VerticalLayout implements V
 	protected static final String NAME = "pantallaDuracionAnalistaTecnico";
 
 	private DuracionService duracionService = DuracionService.getDuracionService();
-
+	
 	
 
 	public PantallaDuracionAnalistaTecnico() {
+		BeanItemContainer<Duracion>duraciones = new BeanItemContainer<>(Duracion.class);
+		duracionService.traerDuraciones().forEach(duracion -> {
+			duraciones.addBean(duracion);
+		});
+		
 		Label titulo = new Label("Gestión de Duraciones");
 		titulo.setStyleName(ValoTheme.LABEL_H1);
 		HorizontalLayout hlTitulo = new HorizontalLayout(titulo);
 		addComponent(hlTitulo);
 		setComponentAlignment(hlTitulo, Alignment.MIDDLE_CENTER);
 
+		ComboBox d = new ComboBox("Duraciones", duraciones);
+		
 		TextField tfDescripcion = new TextField("Descripcion");
+		TextField tfCantidadDias= new TextField("Cantidad dias");
+		this.addComponent(d);
+		
+		
 		// TODO binding a grid.
 	}
 
